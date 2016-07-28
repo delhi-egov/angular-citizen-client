d5e39441ab09f021282dd2a51848d81e728a117d
Vaibhav Sinha <vaibhavsinh@gmail.com>
2016-07-28 10:47:52 +0530

    Bugfixes

--- services/dashboard_service.js
+++ services/dashboard_service.js
@@ -15,11 +15,12 @@ module.exports = function($state, backendClient, authInfo, applicationInfo, appC
         //Picks the processType and currentStage from application
         //Picks the nextState and nextStage from the appConfig
         resumeApplication: function(controller, application) {
+            applicationInfo.application = application;
             var processType = application.type.name;
             var currentStage = application.stage;
-            var nextState = processType + '.' + appConfig[processType][currentStage].nextState;
-            var nextStage = appConfig[processType][currentStage].nextStage;
-            var params = appConfig[processType][currentStage].params;
+            var nextState = processType + '.' + appConfig[processType].stages[currentStage].nextState;
+            var nextStage = appConfig[processType].stages[currentStage].nextStage;
+            var params = appConfig[processType].stages[currentStage].params;
             $state.go(nextState, {
                 processType: processType,
                 currentStage: nextStage,
@@ -38,7 +39,7 @@ module.exports = function($state, backendClient, authInfo, applicationInfo, appC
                     controller.applications = response.data;
                     controller.applications.forEach(function(item, index, arr) {
                         if(item.status === 'PROGRESS') {
-                            backend.getStatus(item.id).then(function(response) {
+                            backendClient.getStatus(item.id).then(function(response) {
                                 controller.applications[index].progress = response.data[0];
                                 resolve(controller.applications);
                             });
