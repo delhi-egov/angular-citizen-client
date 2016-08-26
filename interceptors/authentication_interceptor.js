8c6f524eb55411cffbae32867c8d41c89b938c99
Vaibhav Sinha <vaibhavsinh@gmail.com>
2016-08-26 10:58:35 +0530

    Redirect for authorization failure. Redirect using

--- interceptors/authentication_interceptor.js
+++ interceptors/authentication_interceptor.js
@@ -1,13 +1,16 @@
-module.exports = function($q, $location) {
+module.exports = function($q, $location, $injector) {
     return {
-            response: function(response){
+            response: function(response) {
                 return response || $q.when(response);
             },
             responseError: function(rejection) {
-                if (rejection.status === 401) {
-                    $location.path('/user/login');
+                if (rejection.status === 401 || rejection.status === 403) {
+                    // $location.path('/user/login');
+                    $injector.get('$state').go('user.login');
                 }
                 return $q.reject(rejection);
             }
         };
 };
+
+//http://stackoverflow.com/questions/20230691/injecting-state-ui-router-into-http-interceptor-causes-circular-dependency
